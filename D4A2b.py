a0c489cfae8e1d94639d92d9fde74ba6aa7edd8c
VeronTepes <tickye@msn.com>
2022-02-09 16:02:17 -0700

    did part b

--- D4A2b.py
+++ D4A2b.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb  7 15:07:34 2022
+Created on Wed Feb  9 15:40:35 2022
 
 @author: owner
 """
-
-#part 1
 
 #make the orders.
 orderOne = [34587, ("Learning Python, Mark Lutz", 4, 40.95)]
@@ -15,28 +13,29 @@ orderFour = [88112, ("Einführung in Python3, Bernd Klein", 3, 24.99)]
 #print("Before def")
 
 def quant(list):
-    return list[1][1]
+    return list[1]
 
 def cost(list):
-    return list[1][2]
+    return list[2]
 
 def totalCost(item):
-    #print("Hello")
-    i = cost(item)
-    j = quant(item)
-
+    answer = 0
     ans =  lambda i, j : i * j
     
+    for i in range(1, len(item)):
+        i = cost(item[i])
+        j = quant(item[i])
+        answer += ans(j,i)
+    
+    
 
-    return item[0], ans(i, j)
+    return item[0], answer
 
 #print("after def")
 
 #print(totalCost(orderOne))
 #print("before map")
 orders = map(totalCost, (orderOne, orderTwo, orderThree, orderFour))
-
-
 
 print(list(orders))
 

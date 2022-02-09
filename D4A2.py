12fbcabb275fcd327d0eb24fbe5e2c5621d1e2c3
VeronTepes <tickye@msn.com>
2022-02-09 15:36:59 -0700

    finished part 2

--- D4A2.py
+++ D4A2.py
@@ -4,4 +4,36 @@ Created on Mon Feb  7 15:07:34 2022
 
 @author: owner
 """
+#make the orders.
+orderOne = [34587, ("Learning Python, Mark Lutz", 4, 40.95)]
+orderTwo = [98762, ("Programming Python, Mark Lutz", 5, 56.80)]
+orderThree =  [77226, ("Head First Python, Paul Barry", 3, 32.95)]
+orderFour = [88112, ("Einführung in Python3, Bernd Klein", 3, 24.99)]
+print("Before def")
+
+def quant(list):
+    return list[1][1]
+
+def cost(list):
+    return list[1][2]
+
+def totalCost(item):
+    print("Hello")
+    i = cost(item)
+    j = quant(item)
+
+    ans =  lambda i, j : i * j
+    
+
+    return item[0], ans(i, j)
+
+print("after def")
+
+print(totalCost(orderOne))
+print("before map")
+orders = map(totalCost, (orderOne, orderTwo, orderThree, orderFour))
+
+
+
+print(list(orders))
 
